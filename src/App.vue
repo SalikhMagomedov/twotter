9f2d87a940f20c87e28ce9fdc2568ca78c327702
Salikh Magomedov <salikh.magomedov@icloud.com>
2021-01-04 15:10:10 +0300

    Template Conditionals, Loops and Events

--- src/App.vue
+++ src/App.vue
@@ -1,51 +1,15 @@
 <template>
 <div id="app">
-  @{{ user.username }} - {{ fullName }}
-  <strong>Followers: </strong> {{ followers }}
-  <button @click="followUser">
-    Follow
-  </button>
+  <user-profile/>
 </div>
 </template>
 
 <script>
+import UserProfile from './components/UserProfile.vue'
 
 export default {
   name: 'App',
-  data() {
-    return {
-      isLoading: false,
-      followers: 0,
-      user: {
-        id: 1,
-        username: 'sm',
-        firstName: 'Salikh',
-        lastName: 'Magomedov',
-        email: 'salikh.magomedov@icloud.com',
-        isAdmin: true
-      }
-    }
-  },
-  watch: {
-    followers(newFollowerCount, oldFollowerCount) {
-      if (oldFollowerCount < newFollowerCount) {
-        console.log(`${this.user.username} has gained a follower!`)
-      }
-    }
-  },
-  computed: {
-    fullName() {
-      return `${this.user.firstName} ${this.user.lastName}`
-    }
-  },
-  methods: {
-    followUser() {
-      this.followers++
-    }
-  },
-  mounted() {
-    this.followUser()
-  }
+  components: { UserProfile },
 }
 </script>
 
@@ -55,8 +19,7 @@ export default {
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   color: #2c3e50;
-  display: flex;
-  flex-direction: column;
-  text-align: center;
+  min-height: 100vh;
+  background-color: #f3f5fa;
 }
 </style>
