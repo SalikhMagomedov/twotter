a85522e195952db13789a3bbd1d38e0396f68504
Salikh Magomedov <salikh.magomedov@icloud.com>
2021-01-04 19:11:33 +0300

    Forms and Handling User Input

--- src/components/UserProfile.vue
+++ src/components/UserProfile.vue
@@ -8,6 +8,23 @@
       <div class="user-profile__follower_count">
         <strong>Followers: </strong> {{ followers }}
       </div>
+      <form class="user-profile__create-twoot" @submit.prevent="createNewTwoot">
+        <label for="newTwoot"><strong>New Twoot</strong></label>
+        <textarea id="newTwoot" rows="4" v-model="newTwootContent"></textarea>
+
+        <div class="user-profile__create-twoot-type">
+          <label for="newTwootType"><strong>Type: </strong></label>
+          <select name="" id="newTwootType" v-model="selectedTwootType">
+            <option :value="option.value" v-for="(option, index) in twootTypes" :key="index">
+              {{ option.name }}
+            </option>
+          </select>
+        </div>
+
+        <button>
+          Twoot!
+        </button>
+      </form>
     </div>
     <div class="user-profile__twoots-wrapper">
       <twoot-item 
@@ -28,6 +45,12 @@ export default {
   components: { TwootItem },
   data() {
     return {
+      newTwootContent: '',
+      selectedTwootType: 'instant',
+      twootTypes: [
+        { value: 'draft', name: 'Draft' },
+        { value: 'instant', name: 'Instant Twoot' }
+      ],
       followers: 0,
       user: {
         id: 1,
@@ -61,6 +84,15 @@ export default {
     },
     toggleFavorite(id) {
         console.log(`Favorite Twoot #${id}`);
+    },
+    createNewTwoot() {
+      if (this.newTwootContent && this.selectedTwootType !== 'draft') {
+        this.user.twoots.unshift({
+          id: this.user.twoots.length + 1,
+          content: this.newTwootContent
+        })
+      }
+      this.newTwootContent = ''
     }
   },
   mounted() {
@@ -103,5 +135,11 @@ h1 {
 .user-profile__twoots-wrapper {
   display: grid;
   grid-gap: 10px;
+}
+
+.user-profile__create-twoot {
+  padding-top: 20px;
+  display: flex;
+  flex-direction: column;
 }
 </style>
